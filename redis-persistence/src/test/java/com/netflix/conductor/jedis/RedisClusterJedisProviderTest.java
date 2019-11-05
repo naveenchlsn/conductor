ce19380e4f09f959a60dc15455baad86daa5fa90
naveen chalasani <naveen.chalasani@swiggy.in>
2019-11-05 12:37:30 +0530

    Adds test case for non null password

--- redis-persistence/src/test/java/com/netflix/conductor/jedis/RedisClusterJedisProviderTest.java
+++ redis-persistence/src/test/java/com/netflix/conductor/jedis/RedisClusterJedisProviderTest.java
@@ -10,6 +10,7 @@ import org.mockito.Mockito;
 import redis.clients.jedis.JedisCommands;
 import redis.clients.jedis.exceptions.JedisNoReachableClusterNodeException;
 
+import java.net.UnknownHostException;
 import java.util.Arrays;
 
 import static org.mockito.Mockito.when;
@@ -20,11 +21,21 @@ public class RedisClusterJedisProviderTest {
     @Test
     public void onGetWithNullPasswordInHosts_commandsIsNotNull() {
         HostSupplier hostSupplier = Mockito.mock(HostSupplier.class);
-        Host mockHost = new Host("hostname",123,"region");
+        Host mockHost = new Host("hostname", 123, "region");
         when(hostSupplier.getHosts()).thenReturn(Arrays.asList(mockHost));
         RedisClusterJedisProvider provider = new RedisClusterJedisProvider(hostSupplier);
         JedisCommands commands = provider.get();
         Assert.assertNotNull(commands);
+    }
+
+    @Test(expected = UnknownHostException.class)
+    public void onGetWithPasswordInHosts_commandsIsNotNull() {
+        HostSupplier hostSupplier = Mockito.mock(HostSupplier.class);
+        Host mockHost = new Host("hostname", 123, "region", Host.Status.Up, "hastag", "password");
+
+        when(hostSupplier.getHosts()).thenReturn(Arrays.asList(mockHost));
+        RedisClusterJedisProvider provider = new RedisClusterJedisProvider(hostSupplier);
+        JedisCommands commands = provider.get();
     }
 
 }
